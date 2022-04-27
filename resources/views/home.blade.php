866b152598816209a12a5bcbf445b290586ff4cd
twotimesgi <luigibardellagerbi@mail.com>
2022-04-27 16:11:29 +0200

    Header

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('templates.default')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="hero">
+        <div class="container">
+            <div class="title">CURRENT SERIES</div>
+        </div>
+    </div>
+    <div class="comics-list">
+        <div class="container"></div>
+    </div>
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+<h2>Foooter</h2>
+</footer>
